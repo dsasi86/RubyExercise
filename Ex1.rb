594111c355f0f9ad487c63c3f9282f6fb0830f3d
dsasi86 <95671014+dsasi86@users.noreply.github.com>
2021-12-07 15:16:17 +0530

    Update Ex1.rb

--- Ex1.rb
+++ Ex1.rb
@@ -1,1 +1,5 @@
-puts " hello world"
+# study Drills
+inches = 12
+pounds = 180
+puts "Convert inches #{inches}, to centimeters #{2.54 * inches}."
+puts "Convert pounds #{pounds}, to Kilograms #{0.453592 * pounds}."

00726baf1998a4af466fbc5fdc20140bd4bd9e7d
dsasi86 <95671014+dsasi86@users.noreply.github.com>
2021-12-10 12:37:22 +0530

    Add files via upload

---
+++ Ex7.rb
@@ -0,0 +1,11 @@
+# Formatter inside Formatter
+# Assign multiple values to a varialbe use %{}
+Formatter = "%{First} %{Second} %{Third} %{Fourth}"
+
+puts Formatter % {First:1, Second:2, Third:3, Fourth:4} # values assigned are numbers doesn't need ""
+puts Formatter % {First: "One", Second:"two" ,Third:"Three", Fourth:"Four"} # Values assigned are string must use "" or ''
+puts Formatter % {First: true, Second: false, Third: true, Fourth:false} # values assigned are not string its boolean values
+puts Formatter % {First: "ruby is simple language",
+                  Second: "i started to learn",
+                  Third: "rightnow feel good",
+                  Fourth: "let see what happen"}

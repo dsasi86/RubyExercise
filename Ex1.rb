693fc3a8c82cdd8bab31bd5f15a4732786e7f71a
dsasi86 <95671014+dsasi86@users.noreply.github.com>
2021-12-13 11:23:24 +0530

    Add files via upload

--- Ex1.rb
+++ Ex1.rb
@@ -1,5 +1,3 @@
-# study Drills
-inches = 12
-pounds = 180
-puts "Convert inches #{inches}, to centimeters #{2.54 * inches}."
-puts "Convert pounds #{pounds}, to Kilograms #{0.453592 * pounds}."
+puts "hello world!"
+puts ("hi welcome")
+puts("simple exercise")

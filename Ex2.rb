ba16e178be64498af343f34e94fc212aba4a1dfd
dsasi86 <95671014+dsasi86@users.noreply.github.com>
2021-12-13 11:25:16 +0530

    Add files via upload

---
+++ Ex2.rb
@@ -0,0 +1,13 @@
+a = "my name is sasikumar" #a is a variable and has string value
+
+x = 9 #x is a variable has interger value
+y = 16 #y is a variable has interger value
+
+z = x + y # z is a variable and has sum value of two variable x & y
+
+puts x # print value of variable x
+puts y # print value of variable y
+
+puts "sum of x & y is #{z}" # print value of variable z
+
+puts Math.sqrt(z) # print square root value of variable z
